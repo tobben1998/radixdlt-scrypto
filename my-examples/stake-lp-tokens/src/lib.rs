444374170f2ba5002c334571b0040aa54dc92924
tobben1998 <tobias.hagehei@gmail.com>
2022-04-14 22:35:21 +0200

    only miss the loop to caluclate rewards and lots of syntax error. I think logic should be correct

--- my-examples/stake-lp-tokens/src/lib.rs
+++ my-examples/stake-lp-tokens/src/lib.rs
@@ -9,8 +9,15 @@ pub struct StakerData{
     amount: Decimal,
 }
 
+#[derive(Debug, TypeId, Encode, Decode, Describe, PartialEq, Eq)]
+pub struct StakedEpoch{
+    epoch: u64,
+    staked: Decimal,
+}
+
 blueprint! {
     struct Stake {
+
       // Define what resources and data will be managed by Stake components
 
         // Authorization for the person making this thing to controll rewards vault.
@@ -22,11 +29,16 @@ blueprint! {
         rewards_vault: Vault, //the owner will set the total rewards amount in here.
 
         
-        stakers: HashMap<Address, StakerData>, 
+        stakers: HashMap<Address, StakerData>,
+        
         //NB! hashmap. only unique keys, so a staker can not stake two times if not taken care of.
         //can not just add an extra element in the hashmap, because only one can exist.
         //better with other strcuts than hashmap, or just update in the stake function based on that?
         //maybe call the witdraw function and then stake function, if they have already staked.
+
+
+        staked_vec: Vec<StakedEpoch> 
+        //update this for every deposit and withdrawel (if no problem with race condition)
     }
 
     impl Stake {
@@ -59,7 +71,13 @@ blueprint! {
 
         pub fn stake(&mut self, stake_tokens: Bucket) -> Bucket{
 
-            //math it with your own adress. how to you get your own adress. 
+                        
+            let curr_epoch=Context::current_epoch();      
+            let amount = stake_tokens.amount(); 
+            assert!(amount > Decimal::zero(), "You need to stake more than zero tokens");
+
+
+            //match it with your own adress. how to you get your own adress. 
             //self refer to the struct stake. want to to caller.Address or somerhing
             match self.stakers.get(){
                 Some(staker) => {
@@ -69,27 +87,30 @@ blueprint! {
                 }
             }
 
-            
-        
-            let amount = stake_tokens.amount(); 
-            assert!(amount > Decimal::zero(), "You need to stake more tah xero tokens");
 
-            let start_time=Context::current_epoch();
-
-        
             // Mint Badge with locked amount and start epoch as metadata
             let badge = ResourceBuilder::new_fungible(DIVISIBILITY_NONE)
                 .metadata("name", "Staker Badge")
                 .metadata("amount", amount.to_string())
-                .metadata("start epoch", start_time.to_string())
+                .metadata("start epoch", curr_epoch.to_string())
                 .flags(MINTABLE | BURNABLE )
                 .badge(self.minter_vault.resource_def(), MAY_MINT | MAY_BURN)
                 .initial_supply_fungible(1);
         
 
+            //Updates how much is staked in stakedVec
+            if curr_epoch == Self.staked_vec.last().epoch {
+                let last=self.staked_vec.pop;
+                self.staked_vec.push([curr_epoch,amount+last.staked]);
+            }
+            else if curr_epoch > Self.staked_vec.last().epoch {
+                self.staked_vec.push([curr_epoch,amount+Self.staked_vec[Self.staked_vec.len()-2].staked]); // add amount of penultimate element
+            }
+
             // store new badge address in the stakers struct
-            self.stakers.insert(badge.address(), StakerData {started_at: start_time, amount: amount});
+            self.stakers.insert(badge.address(), StakerData {started_at: curr_epoch, amount: amount});
             self.stake_vault.put(stake_tokens);
+
             badge
         }
 
@@ -110,17 +131,40 @@ blueprint! {
                 self.minter_vault.authorize(|badge| {
                     badge.burn_with_auth(badge);
                 });
+
                 // update stakers in the component
                 self.stakers.remove(&badge.address());
+
+                //Updates how much is staked in stakedVec
+                if curr_epoch == Self.staked_vec.last().epoch {
+                    let last=self.staked_vec.pop;
+                    self.staked_vec.push([curr_epoch,amount-last.staked]);
+                }
+                else if curr_epoch > Self.staked_vec.last().epoch {
+                    self.staked_vec.push([curr_epoch,amount-Self.staked_vec[Self.staked_vec.len()-2].staked]);// subtract amount of penultimate element
+                }
+
+                
                 
 
                 bucket.put(self.stake_vault.take(staker_data.amount));
-                bucket.put(self.rewards_vault.take(1)); //put in rewards. do this based on the math
+
+                let rewards=0;
+                let total_rewards_epoch= 100; //this should be made decide in new or something
+
+
                 //do some math to calculate rewards based on total amount staked in pool,
                 // amount user have staked and time he started staking compared to time now and 
                 //how manye total tokens have been staked in the different epochs.
+
+                //loop thourgh staked and constisusly calcuate total share of the pool and and and that to your rewards. (if tha hashmap is possible to make regarding race conditions.)
             
 
+
+
+                bucket.put(self.rewards_vault.take(1)); //put in rewards. do this based on the math
+                
+                
                 },
                 None => {
                     info!("no mints found with provided badge");
